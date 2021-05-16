3341ed90e6037c012ca9a70555acd4e2f1e4d13e
Shaun Lai <vash988@yahoo.com.tw>
2021-05-16 12:14:37 +0800

    reduce coding line but get same result

--- guess_numberV2.py
+++ guess_numberV2.py
@@ -7,11 +7,13 @@ while True:                 #進入while 並且不斷重複
 	num = input('猜猜看數字 1 - 100:')
 	num = int(num)    #將input的字串型別轉換為整數
 	if num == r:
-		print('終於猜對了')
+		count = count + 1
+		print('終於猜對了','這是你猜得第' ,count ,'次')	
 		break          #印出後從這邊斷掉While迴圈
 	elif num > r:
 		count = count + 1
-		print('你猜得比較大喔', '這是你猜得第' ,count ,'次')
+		print('你猜得比較大喔')
 	elif num < r:
 		count = count + 1
-		print('你猜得比較小喔', '這是你猜得第' ,count ,'次')
+		print('你猜得比較小喔')
+	print('這是你猜得第' ,count ,'次')  #放在elif架構之外,減少行數
